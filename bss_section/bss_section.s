7ae73c13d17fbb6dc3da75b41f02e9e1f3f4314f
Christian <christianparadix@gmail.com>
2024-06-16 16:50:57 -0300

    bss section

--- bss_section/bss_section.s
+++ bss_section/bss_section.s
@@ -4,5 +4,19 @@
 .globl _start
 _start:
     # read syscall for taking user input
+    movl $3,%eax # syscall number
+    movl $0,%ebx # file descriptor for input
+    movl $buffer,%ecx # buffer pointer
+    movl $15,%edx     # buffer size
+    int $0x80
     # write syscall for printing the buffer
+    movl $4,%eax
+    movl $1,%ebx
+    movl $buffer,%ecx
+    movl $15,%edx
+    int $0x80
+    # exit syscall to exit the program
+    movl $1,%eax
+    movl $0,%ebx
+    int $0x80
     
